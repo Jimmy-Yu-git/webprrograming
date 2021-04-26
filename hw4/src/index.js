4bb9151556da964601bda28a110b29401537fcb0
Jimmy-Yu-git <77708964+Jimmy-Yu-git@users.noreply.github.com>
2021-04-26 17:02:10 +0800

    Create index.js

---
+++ hw4/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import FakeSheet from "./components/FakeSheet";
+//import index from "./js/index";
+ReactDOM.render(
+    <FakeSheet />,
+    document.getElementById("root")
+);
